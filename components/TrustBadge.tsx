ea77d682b1c286fbaf5acc4d70ec403bc194d8ab
Aidan1223f <aidan.nt76@gmail.com>
2025-12-06 09:37:53 -0800

    removed blindly trustd lol

--- components/TrustBadge.tsx
+++ components/TrustBadge.tsx
@@ -4,7 +4,6 @@ export function TrustBadge() {
   return (
     <section className="py-12 px-6">
       <div className="max-w-6xl mx-auto text-center">
-        <p className="text-lg text-tertiary">Blindly trusted by</p>
       </div>
     </section>
   );
