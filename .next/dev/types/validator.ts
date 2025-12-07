095569bdb851dbdf1ee82daa0e62c626434c22af
Aidan1223f <aidan.nt76@gmail.com>
2025-12-07 01:22:11 -0800

    package scraping script cmd

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -56,6 +56,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../app/api/scrape-techcrunch/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/scrape-techcrunch">> = Specific
+  const handler = {} as typeof import("../../../app/api/scrape-techcrunch/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/api/upload-resume/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/upload-resume">> = Specific
